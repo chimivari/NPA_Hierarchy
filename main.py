52e2cd2840f51dc1eea1b700c36db627a028edcf
andeol_vella <chimivari@gmail.com>
2026-04-29 18:03:10 +0200

    reduce method ok

--- main.py
+++ main.py
@@ -12,6 +12,12 @@ class symbol:
     def __repr__(self):
         return f'{self.who}{self.output}'
     
+    def __eq__(self, value):
+        return self.who == value.who and self.output == value.output
+    
+    def __ne__(self, value):
+        return not (self == value)
+    
     def same_who(self, who):
         return self.who == who
 
@@ -23,7 +29,11 @@ class identity:
         return 'I'
 
 class group:
-    def __init__(self, s):
+    def __init__(self, s = None):
+        if s == None:
+            self.first_b_index = 0
+            self.symbols = []
+            return
         if isinstance(s, identity):
             self.symbols = [s]
             self.first_b_index = 1
@@ -68,7 +78,7 @@ class group:
     def append(self, s: symbol):
         if s.same_who('A'):
             self.first_b_index += 1
-        elif s.same_who('B') and self.symbols[-1].same_who('A'):
+        elif s.same_who('B') and (len(self) == 0 or self.symbols[-1].same_who('A')):
             self.first_b_index = len(self)
         self.symbols.append(s)
 
@@ -81,6 +91,39 @@ class group:
         Bs.reverse()
         self.symbols = As + Bs
         return self
+
+    def reduce_with(self, other):
+        if isinstance(self[0], identity):
+            return other
+        elif isinstance(other[0], identity):
+            return self
+        rst = group()
+        # Réduire les A
+        i1 = self.first_b_index - 1
+        i2 = 0
+        while i1 >= 0 and i2 < other.first_b_index:
+            s1 = self[i1]
+            s2 = other[i2]
+            if s1 != s2:
+                break
+            i1 -= 1
+            i2 += 1
+        rst.symbols = self[:i1 + 1] + other[i2:other.first_b_index]
+        rst.first_b_index = len(rst)
+        # Réduire les B
+        j1 = len(self) - 1
+        j2 = other.first_b_index
+        while j1 >= self.first_b_index and j2 < len(other):
+            s1 = self[j1]
+            s2 = other[j2]
+            if s1 != s2:
+                break
+            j1 -= 1
+            j2 += 1
+        rst.symbols += self[self.first_b_index:j1 + 1] + other[j2:len(self)]
+        if len(rst) == 0:
+            return group(identity())
+        return rst
 
 def generate_combinations(N, M, d):
     # Création des listes de labels
@@ -129,10 +172,9 @@ def make_moment_matrix(N, M, d):
     col_labels = [group(identity())] 
     for i in range(1, d + 1):
         col_labels += generate_combinations(N, M, i)
-    for l in col_labels:
-        print(l, l.first_b_index)
     row_labels = copy.deepcopy(col_labels)
     row_labels = [l.dagger() for l in row_labels]
+
     return col_labels, row_labels
     
 N, M, d = 2, 2, 2
